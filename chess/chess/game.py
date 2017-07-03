5550d2a3d32b14dd35fbda0f0a6a2bb8c6a4fd18
CCThomas <ChristopherCThomas94@gmail.com>
2017-07-03 17:44:47 -0500

    Add Developer Console

--- chess/chess/game.py
+++ chess/chess/game.py
@@ -1,12 +1,43 @@
-from chess.objects.board import Board
-from chess.objects.pieces import King
-
 def start():
     print('==========================')
     print('Python 3 Chess Application')
     print('==========================')
 
-    print('Quick Test of Board')
-    my_board = Board()
-    my_king = King()
 
+#
+# Developer Mode
+#
+def dev_quit():
+    import sys
+    sys.exit(0)
+
+
+def dev_test():
+    from chess.test import console
+    console.start()
+
+
+def dev_usage():
+    print('"quit" -> Quits Program')
+    print('"test" -> Run Testing Console')
+
+
+commands = {
+    'help': dev_usage,
+    'quit': dev_quit,
+    'test': dev_test
+}
+
+
+def dev_console():
+    print('==========================')
+    print('Python 3 Chess Application')
+    print('      Developer Mode')
+    print('==========================')
+
+    while True:
+        user_input = input("> ")
+        if user_input in commands:
+            commands[user_input]()
+        else:
+            print('type "help" to print usage')

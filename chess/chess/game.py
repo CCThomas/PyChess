e207e19cd595cb7997da10c389aa9b9b2f119708
CCThomas <ChristopherCThomas94@gmail.com>
2017-07-03 17:07:12 -0500

    Ad Quick Test of Board and King Piece

--- chess/chess/game.py
+++ chess/chess/game.py
@@ -1,4 +1,12 @@
+from chess.objects.board import Board
+from chess.objects.pieces import King
+
 def start():
     print('==========================')
     print('Python 3 Chess Application')
     print('==========================')
+
+    print('Quick Test of Board')
+    my_board = Board()
+    my_king = King()
+

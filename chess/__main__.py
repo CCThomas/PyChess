db0c9b85819e51a85e7dfb7cecd627f5e93558fc
CCThomas <ChristopherCThomas94@gmail.com>
2017-07-03 16:56:17 -0500

    Initialize Directory Structures

---
+++ chess/__main__.py
@@ -0,0 +1,8 @@
+# author: Christopher Thomas ~ github.com/CCThomas
+# author: Brandon Wong ~ github.com/BW0ng
+# created: July 3td 2017
+# version: 0.0.0
+
+from chess import game
+
+game.start()
---
+++ chess/chess/game.py
@@ -0,0 +1,4 @@
+def start():
+    print('==========================')
+    print('Python 3 Chess Application')
+    print('==========================')

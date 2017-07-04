85b94130400260b44d3a204608f568ab91b47902
CCThomas <ChristopherCThomas94@gmail.com>
2017-07-04 11:49:24 -0500

    Add more Test

--- chess/chess/test/console.py
+++ chess/chess/test/console.py
@@ -1,6 +1,27 @@
 def test_board():
     from chess.objects.board import Board
+    from chess.objects.player import Player
     my_board = Board()
+    player_1 = Player('red')
+    player_1.init_pieces("north")
+    player_2 = Player('blue')
+    player_2.init_pieces('south')
+
+    p1_pieces = player_1.get_pieces()
+    for piece in p1_pieces:
+        my_board.place_piece(p1_pieces[piece].get_name(), p1_pieces[piece].get_letter(),
+                             p1_pieces[piece].get_number())
+
+
+    p2_pieces = player_2.get_pieces()
+    for piece in p2_pieces:
+        my_board.place_piece(p2_pieces[piece].get_name(), p2_pieces[piece].get_letter(),
+                                 p2_pieces[piece].get_number())
+
+    my_board.print_board()
+
+def test_gui():
+    from chess.gui import playground
 
 def test_quit():
     import sys
@@ -15,6 +36,7 @@ def test_usage():
 
 commands = {
     'board': test_board,
+    'gui': test_gui,
     'help': test_usage,
     'quit': test_quit
 }
